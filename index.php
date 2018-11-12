39b75653d2cbb54128a0ad3e96d41294c7ee306e
Jon Williams <jon.williams@cae.com>
2018-11-12 11:02:45 +0000

    css update

--- index.php
+++ index.php
@@ -11,124 +11,138 @@
 </head>
 <body class="background">
 
+
   <div id="header-w">
 		<div id="header">
       <div class="logo-container">
           <div class="logo"><a href="<?php echo $this->baseurl ?>/"><img src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/logo.png" alt="logo" ></a></div>
-      </div>
-			<nav class="clearfix">
-				<div id="nav"><jdoc:include type="modules" name="mainmenu" style="none" /></div>
-			</nav>
-		</div> <!-- end header -->
-	</div><!-- end header-w -->
+      </div> <!-- .logo-container -->
+			<nav class="clearfix"><div id="nav"><jdoc:include type="modules" name="mainmenu" style="none" /></div></nav>
+		</div> <!-- #header -->
+	</div><!-- #header-w -->
 
   <!-- Slideshow -->
-  <div id="firstbox" >
- 	<center>
-	<div id="slideshow"><?php if ($this->params->get( 'slidehome' )) : ?>
-		<?php $app = JFactory::getApplication(); $menu = $app->getMenu(); $lang = JFactory::getLanguage();
-			if ($menu->getActive() == $menu->getDefault($lang->getTag())) : ?>
-			<?php include "slideshow/slideshow.php"; ?><?php endif; ?>
-		<?php else : ?>
-			<?php include "slideshow/slideshow.php"; ?><?php endif; ?>
-	</div>
-	</center>
-</div>
-<!-- END Slideshow -->
+  <div id="firstbox">
+    <center>
+      <div id="slideshow">
+        <?php if ($this->params->get( 'slidehome' )) : ?>
+        <?php $app = JFactory::getApplication(); $menu = $app->getMenu(); $lang = JFactory::getLanguage();
+        if ($menu->getActive() == $menu->getDefault($lang->getTag())) : ?>
+        <?php include "slideshow/slideshow.php"; ?><?php endif; ?>
+        <?php else : ?>
+        <?php include "slideshow/slideshow.php"; ?><?php endif; ?>
+      </div> <!-- #slideshow -->
+    </center>
+  </div> <!-- #firstbox -->
 
-<div class="container-fluid" id="relative">
-<div id="wrapper-w"><?php include 'html/template.php';?>
-<div id="wrapper">
+  <div>&nbsp;</div>
 
-	<?php $app = JFactory::getApplication(); $menu = $app->getMenu(); $lang = JFactory::getLanguage(); if ($menu->getActive() == $menu->getDefault($lang->getTag())) : ?>
-	<?php if($this->countModules('top1') or $this->countModules('top2') or $this->countModules('top3')) : ?>
-		<div id="top" class="clearfix">
-			<div class="top1 hvr-rectangle-out"><jdoc:include type="modules" name="top1" style="xhtml" /></div>
-			<div class="top2 hvr-rectangle-out"><jdoc:include type="modules" name="top2" style="xhtml" /></div>
-			<div class="top3 hvr-rectangle-out"><jdoc:include type="modules" name="top3" style="xhtml" /></div>
-		</div>
-	<?php endif; ?><?php endif; ?>
+  <div class="container-fluid" id="relative">
 
-	<div id="main-content">
-    <?php if($this->countModules('left') xor $this->countModules('right')) $maincol_sufix = '_one';
-	  elseif(!$this->countModules('left') and !$this->countModules('right'))$maincol_sufix = '_none';
-	  else $maincol_sufix = '_both'; ?>
-<!-- Left Sidebar -->
-    <?php if($this->countModules('left') and JRequest::getCmd('layout') != 'form') : ?>
-			<div id="leftbar-w">
-			<div id="sidebar"><jdoc:include type="modules" name="left" style="hq" /></div>
-			</div>
-    <?php endif; ?>
+    <div id="wrapper-w"><?php include 'html/template.php';?>
 
-<!-- Center -->
-	<div id="centercontent<?php echo $maincol_sufix; ?>">
-		<div class="clearpad"><jdoc:include type="component" /></div>
-		<?php if ($this->countModules('breadcrumb')) : ?>
-        <jdoc:include type="modules" name="breadcrumb"  style="none"/>
-        <?php endif; ?><?php $app = JFactory::getApplication(); $menu = $app->getMenu(); $lang = JFactory::getLanguage();
-		if ($menu->getActive() == $menu->getDefault($lang->getTag())) : include "html/com_content/archive/function.php"; ?><?php endif; ?>
-	</div>
+      <div id="wrapper">
 
-<!-- Right Sidebar -->
-    <?php if($this->countModules('right') and JRequest::getCmd('layout') != 'form') : ?>
-			<div id="rightbar-w">
-			<div id="sidebar"><jdoc:include type="modules" name="right" style="hq" /></div>
-			</div>
-    <?php endif; ?>
+        <?php $app = JFactory::getApplication(); $menu = $app->getMenu(); $lang = JFactory::getLanguage(); if ($menu->getActive() == $menu->getDefault($lang->getTag())) : ?>
+        <?php if($this->countModules('top1') or $this->countModules('top2') or $this->countModules('top3')) : ?>
 
-		<div class="clr"></div>
-    </div>
+        <div id="top" class="clearfix">
+          <div class="top1 hvr-rectangle-out"><jdoc:include type="modules" name="top1" style="xhtml" /></div>
+          <div class="top2 hvr-rectangle-out"><jdoc:include type="modules" name="top2" style="xhtml" /></div>
+          <div class="top3 hvr-rectangle-out"><jdoc:include type="modules" name="top3" style="xhtml" /></div>
+        </div> <!-- #top -->
 
-<!-- TABS -->
+        <?php endif; ?><?php endif; ?>
 
-<div id="box">
-<jdoc:include type="modules" name="position-0" style="hq" />
-<div id="right"><?php if ($this->countModules('newsflash')) : ?><jdoc:include type="modules" name="newsflash" style="xhtml" /><?php endif; ?></div>
+        <div id="main-content">
 
-<?php if (($this->countModules('tab1')) || ($this->countModules('tab2')) || ($this->countModules('tab3'))) :?>
-<div class="tabs" id="left">
+          <?php if($this->countModules('left') xor $this->countModules('right')) $maincol_sufix = '_one';
+          elseif(!$this->countModules('left') and !$this->countModules('right'))$maincol_sufix = '_none';
+          else $maincol_sufix = '_both'; ?>
 
-<?php if ($this->countModules('tab1')) : ?>
-       <div class="tab">
-           <input id="tab-4" name="tab-group-2" checked="checked" type="radio">
-           <label for="tab-4">Tab1</label>
-           <div class="content"><jdoc:include type="modules" name="tab1" style="xhtml" /></div>
-       </div>
-<?php endif; ?>
+          <!-- Left Sidebar -->
+          <?php if($this->countModules('left') and JRequest::getCmd('layout') != 'form') : ?>
+          <div id="leftbar-w">
+            <div id="sidebar"><jdoc:include type="modules" name="left" style="hq" /></div>
+          </div>
+          <?php endif; ?>
 
-<?php if ($this->countModules('tab2')) : ?>
-       <div class="tab">
-           <input id="tab-5" name="tab-group-2" type="radio">
-           <label for="tab-5">Tab2</label>
-           <div class="content"><jdoc:include type="modules" name="tab2" style="xhtml" /></div>
-       </div>
-<?php endif; ?>
+          <!-- Center -->
+          <div id="centercontent<?php echo $maincol_sufix; ?>">
+            <div class="clearpad"><jdoc:include type="component" /></div>
+            <?php if ($this->countModules('breadcrumb')) : ?>
+            <jdoc:include type="modules" name="breadcrumb"  style="none"/>
+            <?php endif; ?><?php $app = JFactory::getApplication(); $menu = $app->getMenu(); $lang = JFactory::getLanguage();
+            if ($menu->getActive() == $menu->getDefault($lang->getTag())) : include "html/com_content/archive/function.php"; ?><?php endif; ?>
+          </div> <!-- #centercontent -->
 
-<?php if ($this->countModules('tab3')) : ?>
-        <div class="tab">
-           <input id="tab-6" name="tab-group-2" type="radio">
-           <label for="tab-6">Tab3</label>
-           <div class="content"><jdoc:include type="modules" name="tab3" style="xhtml" /></div>
-       </div>
-<?php endif; ?>
-   </div>
-<?php endif; ?>
-</div>
+          <!-- Right Sidebar -->
+          <?php if($this->countModules('right') and JRequest::getCmd('layout') != 'form') : ?>
+          <div id="rightbar-w">
+            <div id="sidebar"><jdoc:include type="modules" name="right" style="hq" /></div>
+          </div>
+          <?php endif; ?>
 
-<!-- END TABS -->
-</div><!-- wrapper end -->
-<!-- Start bottomwide -->
-<div id="bottomwide">
-		<div id="bottom" class="clearfix">
-			<div class="user1"><jdoc:include type="modules" name="user1" style="xhtml" /></div>
-			<div class="user2"><jdoc:include type="modules" name="user2" style="xhtml" /></div>
-			<div class="user2"><jdoc:include type="modules" name="user3" style="xhtml" /></div>
-		</div>
-		<div style="display:none;" class="nav_up" id="nav_up"></div>
-</div><!-- End bottomwide -->
-</div><!-- End wrapper-w -->
-</div><!--/.relative -->
-</div><!--/.fluid-container -->
+          <div class="clr"></div>
+
+        </div> <!-- #main-content -->
+
+        <!-- TABS -->
+        <div id="box">
+
+          <jdoc:include type="modules" name="position-0" style="hq" />
+          <div id="right"><?php if ($this->countModules('newsflash')) : ?><jdoc:include type="modules" name="newsflash" style="xhtml" /><?php endif; ?></div>
+          <?php if (($this->countModules('tab1')) || ($this->countModules('tab2')) || ($this->countModules('tab3'))) :?>
+
+          <div class="tabs" id="left">
+
+            <?php if ($this->countModules('tab1')) : ?>
+            <div class="tab">
+              <input id="tab-4" name="tab-group-2" checked="checked" type="radio">
+              <label for="tab-4">Tab1</label>
+              <div class="content"><jdoc:include type="modules" name="tab1" style="xhtml" /></div>
+            </div> <!-- .tab -->
+            <?php endif; ?>
+
+            <?php if ($this->countModules('tab2')) : ?>
+            <div class="tab">
+              <input id="tab-5" name="tab-group-2" type="radio">
+              <label for="tab-5">Tab2</label>
+              <div class="content"><jdoc:include type="modules" name="tab2" style="xhtml" /></div>
+            </div> <!-- .tab -->
+            <?php endif; ?>
+
+            <?php if ($this->countModules('tab3')) : ?>
+            <div class="tab">
+              <input id="tab-6" name="tab-group-2" type="radio">
+              <label for="tab-6">Tab3</label>
+              <div class="content"><jdoc:include type="modules" name="tab3" style="xhtml" /></div>
+            </div> <!-- .tab -->
+            <?php endif; ?>
+
+          </div> <!-- .tabs #left -->
+          <?php endif; ?>
+
+        </div> <!-- # box -->
+
+        <!-- END TABS -->
+
+      </div><!-- #wrapper -->
+
+      <!-- Start bottomwide -->
+      <div id="bottomwide">
+        <div id="bottom" class="clearfix">
+          <div class="user1"><jdoc:include type="modules" name="user1" style="xhtml" /></div>
+          <div class="user2"><jdoc:include type="modules" name="user2" style="xhtml" /></div>
+          <div class="user2"><jdoc:include type="modules" name="user3" style="xhtml" /></div>
+        </div> <!-- #bottom -->
+        <div style="display:none;" class="nav_up" id="nav_up"></div>
+      </div> <!-- #bottomwide -->
+
+    </div> <!-- #wrapper-w -->
+
+  </div><!-- #relative -->
 
 </body>
+
 </html>
